232543e137f06aadb61cbc620815abe9ce7bde50
rinrin <r.chaaaaaan@gmail.com>
2022-12-29 01:04:47 +0900

    first commit

---
+++ src/test/java/com/example/novelist/NovelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.novelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
