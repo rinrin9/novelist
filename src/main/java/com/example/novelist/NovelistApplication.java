c6c374de2b9df06463c9ebdfe9168a5396b2adf2
rinrin <r.chaaaaaan@gmail.com>
2022-12-29 18:36:56 +0900

    トップ画面作成

--- src/main/java/com/example/novelist/NovelistApplication.java
+++ src/main/java/com/example/novelist/NovelistApplication.java
@@ -2,12 +2,18 @@ package com.example.novelist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NovelistApplication {
+public class NovelistApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NovelistApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NovelistApplication.class);
+	}
 }
