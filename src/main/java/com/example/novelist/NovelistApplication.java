c502c768f342190c1e962fc585af2dd798f28c2a
rinrin <r.chaaaaaan@gmail.com>
2023-01-04 22:25:46 +0900

    SNS 連携

--- src/main/java/com/example/novelist/NovelistApplication.java
+++ src/main/java/com/example/novelist/NovelistApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class NovelistApplication extends SpringBootServletInitializer {
@@ -16,4 +21,11 @@ public class NovelistApplication extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(NovelistApplication.class);
 	}
+	
+	@Bean
+    public WebClient rest(ClientRegistrationRepository clients, OAuth2AuthorizedClientRepository authz) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(
+                clients, authz);
+        return WebClient.builder().filter(oauth2).build();
+    }
 }
